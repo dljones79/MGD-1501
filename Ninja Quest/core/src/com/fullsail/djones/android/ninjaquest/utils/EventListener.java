85bcb095c6f0826f15905279262824e96a7c8de8
David Jones <dljones79@fullsail.edu>
2015-02-18 21:36:35 -0600

    Event Listener placeholder set. Spent most of the day trying to figure out my project wouldn't build. Got that fixed.

---
+++ Ninja Quest/core/src/com/fullsail/djones/android/ninjaquest/utils/EventListener.java
@@ -0,0 +1,14 @@
+package com.fullsail.djones.android.ninjaquest.utils;
+
+/**
+ * Created by David Jones on 2/16/15.
+ * IAD 1502
+ * Full Sail University
+ */
+public interface EventListener {
+
+    public void submitScore(int score);
+    public void displayLeaderboard();
+    public void share();
+
+}
--- Ninja Quest/core/src/com/fullsail/djones/android/ninjaquest/utils/GameManagement.java
+++ Ninja Quest/core/src/com/fullsail/djones/android/ninjaquest/utils/GameManagement.java
@@ -1,16 +1,22 @@
 package com.fullsail.djones.android.ninjaquest.utils;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.fullsail.djones.android.ninjaquest.enums.DifficultyLevel;
 import com.fullsail.djones.android.ninjaquest.enums.GameStates;
 
 /**
  * Created by David on 1/22/15.
  */
-public class GameManagement {
+public class GameManagement implements EventListener {
 
     private GameStates currentState;
     private DifficultyLevel difficulty;
     private static GameManagement currentInstance = new GameManagement();
+    private EventListener eventListener;
+
+    public static final String PREFERENCES_NAME = "preferences";
+    private static final String MAX_SCORE_PREFERENCE = "max_score";
 
     public static GameManagement getInstance() {
         return currentInstance;
@@ -44,7 +50,48 @@ public class GameManagement {
         setDifficulty(DifficultyLevel.values()[0]);
     }
 
+    public void setEventListener(EventListener eventListener){
+        this.eventListener = eventListener;
+    }
 
+    @Override
+    public void submitScore(int score) {
+        eventListener.submitScore(score);
+    }
+
+    @Override
+    public void displayLeaderboard() {
+        eventListener.displayLeaderboard();
+    }
+
+    @Override
+    public void share() {
+        eventListener.share();
+    }
+
+    private Preferences getPreferences() {
+        return Gdx.app.getPreferences(PREFERENCES_NAME);
+    }
+
+    public void saveScore(int score) {
+        Preferences preferences = getPreferences();
+        int maxScore = preferences.getInteger(MAX_SCORE_PREFERENCE, 0);
+        if (score > maxScore) {
+            preferences.putInteger(MAX_SCORE_PREFERENCE, score);
+            preferences.flush();
+        }
+    }
+
+    public boolean hasSavedMaxScore() {
+        return getPreferences().getInteger(MAX_SCORE_PREFERENCE, 0 ) > 0;
+    }
+
+    public void submitSavedMaxScore() {
+        Preferences preferences = getPreferences();
+        submitScore(preferences.getInteger(MAX_SCORE_PREFERENCE, 0));
+        preferences.remove(MAX_SCORE_PREFERENCE);
+        preferences.flush();
+    }
 
 
 }
